d9949c3ae711537be89f1baa5ae36b7857e15e5e
Tahooft <tahooft@gmail.com>
2024-03-14 12:18:07 +0100

    tweaking

--- scripts/mpvtube.sh
+++ scripts/mpvtube.sh
@@ -1,9 +1,9 @@
 if curl --head --silent --fail "$(wl-paste)" 2> /dev/null;
  then
-  echo "This page exists."
-	notify-send -t 3000 "Starting video" "$(wl-paste)"
-        mpv $(wl-paste)
+	# notify-send -t 3000 "Starting video" "$(wl-paste)"
+    mpv $(wl-paste)
  else
+    echo "Not a valid url" 
 	notify-send -t 6000 "Not a valid url to use" "$(wl-paste)"
 fi
 
--- scripts/quickedit.sh
+++ scripts/quickedit.sh
@@ -3,24 +3,26 @@
 echo "File to edit or hit <Enter> for exit"
 echo ""
 echo "  0)This script"
-echo "  1)Sway config"
-echo "  2)Waybar config"
-echo "  3).bashrc"
-echo "  4)Ly config"
-echo "  5)mpvtube.sh"
+echo "  1).bashrc"
+echo "  2)Sway config"
+echo "  3)Waybar config"
+echo "  4)nvim config"
+echo "  5)KnowHow"
+echo "  6)mpvtube.sh"
 echo "  x)Exit"
 echo ""
 
 
 # read -n 1
-read n -t 7
+read n 
 case $n in
   0) nvim "$HOME/.config/sway/scripts/quickedit.sh";;
-  1) nvim "$HOME/.config/sway/config";;
-  2) nvim "$HOME/.config/waybar/config";;
-  3) nvim "$HOME/.bashrc";;
-  4) sudo nvim "/etc/ly/config.ini";;
-  5) nvim "$HOME/.config/sway/scripts/mpvtube.sh";;
+  1) nvim "$HOME/.bashrc";;
+  2) nvim "$HOME/.config/sway/config";;
+  3) nvim "$HOME/.config/waybar/config";;
+  4) nvim "$HOME/.config/nvim/";;
+  5) nvim "$HOME/Documents/KnowHow/Linux/";;
+  6) nvim "$HOME/.config/sway/scripts/mpvtube.sh";;
   x) exit;;
   *) echo "invalid option";;
 esac
