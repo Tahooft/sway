bf5a0bf3b6e35f39d3605580537fbb00cec6af68
Tahooft <tahooft@gmail.com>
2024-03-16 18:10:36 +0100

    I/O-config changes

--- scripts/quickedit.sh
+++ scripts/quickedit.sh
@@ -7,7 +7,7 @@ echo "  1).bashrc"
 echo "  2)Sway config"
 echo "  3)Waybar config"
 echo "  4)nvim config"
-echo "  5)KnowHow"
+echo "  5)nvim.txt"
 echo "  6)mpvtube.sh"
 echo "  x)Exit"
 echo ""
@@ -21,8 +21,10 @@ case $n in
   2) nvim "$HOME/.config/sway/config";;
   3) nvim "$HOME/.config/waybar/config";;
   4) nvim "$HOME/.config/nvim/";;
-  5) nvim "$HOME/Documents/KnowHow/Linux/";;
+  5) nvim "$HOME/Documents/KnowHow/Linux/nvim.txt";;
   6) nvim "$HOME/.config/sway/scripts/mpvtube.sh";;
   x) exit;;
   *) echo "invalid option";;
 esac
+
+echo "Bye bye"
